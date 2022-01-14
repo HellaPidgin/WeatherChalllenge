1b192325505fb8902311f33de31a8ac6dba9868d
Samuel <s.okoro07@gmail.com>
2022-01-14 19:22:02 +0100

    Names of Places and GPS Coordinates

---
+++ app/utils/locations.ts
@@ -0,0 +1,22 @@
+export const locations = {
+    London: {
+        name: "London",
+        latitude: 51.5072,
+        longitude: 0.1276
+    },
+    Lagos: {
+        name: "Lagos",
+        latitude: 6.4627, 
+        longitude: 3.3997
+    },
+    Warsaw: {
+        name: "Warsaw",
+        latitude: 52.2297, 
+        longitude: 21.0122
+    },
+    SanFransisco: {
+        name: "San Fransisco",
+        latitude: 37.7749, 
+        longitude: 122.4194
+    }
+}
